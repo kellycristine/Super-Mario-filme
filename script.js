c62cdbbb202f3d5fc6c06cddc802c5bf01c01378
kelly cristine <kcft16094@gmail.com>
2023-08-15 18:56:30 -0300

    supermario

---
+++ script.js
@@ -0,0 +1,16 @@
+/*var base_url = 'https://www.universalpics.com.br/';
+var media_url = base_url;
+var API_URL = 'https://api.universalpics.com.br/';
+var SEARCH_URL = 'https://api.universalpics.com.br//';
+var youtubeAPIKey = 'AIzaSyCd1V2r9O-0ZhgokH_BMQcJqwBkP0XgCq8';
+var youtubePlaylistID = 'PL7SRsnf63wekBCrOIgg2pvVDJ77f9kM4m';
+var dateLocaleStringFacebook = 'pt-BR';
+var dateLocaleStringTagmanager = 'pt-BR';
+var dateLocaleStringStartdate = 'pt_BR';
+
+var DURATIONTEXT = 'Duração:';
+var SOUNDTEXT = 'Som:';
+var SCREENTEXT = 'Tela:';
+var SUBTITLETEXT = 'Legendas:';
+var BONUSTEXT = 'Bônus:';
+*/ 
